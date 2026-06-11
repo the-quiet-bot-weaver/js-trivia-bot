e0b13a970a9a7d856c3e3d442d8676c01712583c
the-quiet-bot-weaver <41kumar51@gmail.com>
2026-06-11 12:51:03 +0530

    Created my first JavaScript Trivia Bot

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Hello! I'm your coding fun fact guide!")
+let botName = "Java";
+let botLocation = "Internet";
+let favoriteLanguage = "Javascript";
+console.log(`My name is ${botName} and I live on ${botLocation}.`)
+console.log(`My favorite programming language is ${favoriteLanguage}.`)
+let codingFact = `Did you know ${favoriteLanguage} was created in just 10 days?`;
+console.log(codingFact)
+codingFact = `Surprise: In ${favoriteLanguage}, 0.1 + 0.2 is not exactly 0.3, it equals 0.30000000000000004!`;
+console.log(codingFact)
+codingFact =`Fun fact: In ${favoriteLanguage}, NaN is actually of type 'number'!`;
+console.log(codingFact)
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`)

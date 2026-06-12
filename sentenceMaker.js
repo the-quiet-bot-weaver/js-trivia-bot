ca5b5bc6301f877686771190266c34d5916533e6
the-quiet-bot-weaver <41kumar51@gmail.com>
2026-06-12 16:39:26 +0530

    Added senteceMaker Lab practical

---
+++ sentenceMaker.js
@@ -0,0 +1,24 @@
+let adjective, noun ,verb ,place, adjective2, noun2;
+// First Story
+adjective = "brave";
+noun = "knight";
+verb = "fought";
+place = "enchanted forest";
+adjective2 = "mysterious";
+noun2 = "castle";
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("First story: " + firstStory + "\n");
+
+// Second Story
+adjective = "playful";
+noun = "wizard";
+verb = "danced";
+place = "crystal tower";
+adjective2 = "sparkling";
+noun2 = "potion";
+
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("Second story: " + secondStory);
